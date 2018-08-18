2b1d92c19d478baea4c84e1ce5be9905629f78e1
jonathan.kay <jonathan.kay@nwea.org>
2018-08-17 21:54:14 -0700

    added aboutme

--- src/components/common/Toolbar/Toolbar.js
+++ src/components/common/Toolbar/Toolbar.js
@@ -12,7 +12,7 @@ const toolbar = props => (
         <div className="toolbar__toggle-button">
           <DrawerToggleButton click={props.drawerClickHandler} />
         </div>
-        <div className="toolbar__logo"><Link to='/'>who?</Link></div>
+        <div className="toolbar__logo"><Link to='/'>home</Link></div>
         <div className="spacer" />
         <div className="toolbar_navigation-items">
             <ul>
@@ -20,6 +20,7 @@ const toolbar = props => (
               <li className='toolbar_navigation-items_dropdown'>movies<span>▼</span></li>
               <li><Link to='/music'>music</Link></li>
               <li><Link to='/photos'>photography</Link></li>
+              <li><Link to='/about'>who?</Link></li>
             </ul>
         </div>
     </nav>
